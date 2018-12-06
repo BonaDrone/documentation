fcbb170e75f18c176a412d81d99899b68bc02a1c
Juan Gallostra <juangallostra@gmail.com>
2018-12-06 09:18:41 +0100

    Document code

--- extras/scripts/set_params.py
+++ extras/scripts/set_params.py
@@ -24,12 +24,20 @@ args = parser.parse_args()
 
 
 def set_positioning_board(has_positioning_board, serial_com, print_data = True):
+	"""
+	Set if the Mosquito has the positoning board (1 if True and 0 if False)
+	and send it to the Mosquito via an MSP message
+	"""
 	data = serialize_SET_POSITIONING_BOARD(has_positioning_board);
 	serial_com.write(data)
 	if print_data:
 		print("Data to send: {}".format(data))
 
 def set_mosquito_version(mosquito_version, serial_com, print_data = True):
+	"""
+	Set the version of the Mosquito (90 if mosquito_version is 1 and 150 if
+	mosquito version is 0) and send it to the Mosquito via an MSP message
+	"""
 	data = serialize_SET_MOSQUITO_VERSION(mosquito_version);
 	serial_com.write(data)
 	if print_data:
@@ -37,7 +45,15 @@ def set_mosquito_version(mosquito_version, serial_com, print_data = True):
 
 def set_rate_pid(constants, serial_com, print_data = True):
 	"""
-	Constants should be a tuple of 6 values
+	Set the Mosquito's PID constants via an MSP message.
+	Constants should be a tuple of 7 values. In order:
+	- gyroRollPitchP (Rate PID)
+	- gyroRollPitchI (Rate PID)
+	- gyroRollPitchD (Rate PID)
+	- gyroYawP       (Rate PID)
+	- gyroYawI       (Rate PID)
+	- demandsToRate  (Rate PID)
+	- levelP         (Level PID)
 	"""
 	data = serialize_SET_PID_CONSTANTS(*constants)
 	serial_com.write(data)
@@ -45,8 +61,13 @@ def set_rate_pid(constants, serial_com, print_data = True):
 		print("Data to send: {}".format(data))
 
 def main(pos_board_param, mosquito_version_param, constants_param):
+	"""
+	Connect with the Mosquito via Serial and send the parameters passed
+	as command line arguments
+	"""
 	# Serial communication object
 	serial_com = serial.Serial(PORT, BAUDRATE)
+	# process arguments and send appropriate messages
 	if pos_board_param is not None:
 		set_positioning_board(pos_board_param, serial_com)
 	if mosquito_version_param is not None:
