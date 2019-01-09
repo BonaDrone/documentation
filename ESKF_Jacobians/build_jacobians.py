0af041223a2ecf9ff8683e24e3c32425b65fd7a2
Juan Gallostra <juangallostra@gmail.com>
2019-01-09 16:15:47 +0100

    Add script to transform matlab-computed Jacobians to Hackflight's jacobians

---
+++ ESKF_Jacobians/build_jacobians.py
@@ -0,0 +1,70 @@
+# Script to build Jacobians for BaonaDrone's ESKF implementation 
+#
+# Author: Juan Gallostra jgallostra<at>bonadrone.com
+# Date: 01-09-2018
+#
+# Steps:
+#  1. Run the Matlab script that builds the nominal and error state jacobians (Jacobians.m)
+#  2. Copy the desired Jacobian in a plain text file (.txt)
+#  3. Update the Matlab <-> Hackflight relations in correspondences.py
+#  4. Run this script with the previous text file as input 
+#
+# Notation:
+#   1. State:  
+# 		- position: 	x[0], x[1], x[2]
+#   	- valocity: 	x[3], x[4], x[5]
+#   	- quaternion: 	x[6], x[7], x[8], x[9]
+#		- gyro bias:	x[10], x[11], x[12]
+
+import substitutions as s
+import argparse
+
+# Command line arguments
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-i','--input', type=str, action='store', help="input text file")
+parser.add_argument('-o','--output', type=str, action='store', help="output text file")
+
+args = parser.parse_args()
+
+def main(input_file, output_file):
+	content = None
+	rows = 0
+	cols = 0
+	# read data as computed with Matlab
+	with open(input_file) as f:
+		content = [x.strip() for x in f.readlines()]
+	rows = len(content)
+	content = [i.split(",") for i in content]
+	cols = len(content[0])
+	# change powers and var names
+	jacobian = []
+	for line in content:
+		curr_row = []
+		for element in line:
+			if element[0] == "[":
+				element = element[1:]
+			if element[-1] == "]":
+				element = element[:-1]
+			replaced_powers = s.replace_powers(element)
+			mod_element = s.change_var_names(replaced_powers)
+			curr_row += [mod_element]
+		jacobian += [curr_row]
+	# build jacobian in Hackflight format (Adding I and multiplying by dt)
+	for i in range(rows):
+		for j in range(cols):
+			if jacobian[i][j] not in ["0", " 0", "0 "]:
+				jacobian[i][j] = "("+jacobian[i][j]+")*dt"
+			if i==j:
+				jacobian[i][j] = "Fx["+str(i*rows+j)+"] = 1 + "+jacobian[i][j]+";\n"
+			else:
+				jacobian[i][j] = "Fx["+str(i*rows+j)+"] = "+jacobian[i][j]+";\n"
+	# Write to a txt file:
+	with open(output_file, 'w') as file:
+		for j in range(cols):
+			file.write("// "+str(j+1)+" column\n")
+			for i in range(rows):
+				file.write(jacobian[i][j])
+
+if __name__ == '__main__':
+	main(args.input, args.output)
---
+++ ESKF_Jacobians/substitutions.py
@@ -0,0 +1,118 @@
+# Matlab <-> Hackflight ESKF relations 
+#
+# Author: Juan Gallostra jgallostra<at>bonadrone.com
+# Date: 01-09-2018
+
+# Matlab to Hackflight substitutions
+JACOBIAN = "Fx"
+
+SUBS = {
+	"qw" : "x[6]", # x[0-5] are positions and velocities
+	"qx" : "x[7]",
+	"qy" : "x[8]",
+	"qz" : "x[9]",
+	"wsx" : "_rates[0]",
+	"wsy" : "_rates[1]",
+	"wsz" : "_rates[2]",
+	"asx" : "gs[0]", # to be defined
+	"asy" : "gs[1]",
+	"asz" : "gs[2]",
+	"wbx" : "x[10]",
+	"wby" : "x[11]",
+	"wbz" : "x[12]",
+}
+
+
+# required constants
+NORMAL_CHARS = [" ","+","-","/", "*", "(", ")"]
+NUM_CHARS = [str(i) for i in range(10)]
+POWER = "^"
+
+# Helper functions for specific Matlab to Hackflight replacements
+def replace_powers(element):
+	"""
+	Takes an individual element from the jacobian
+	and replaces all the powers in the form a^n by a*a*...*a
+	"""
+	mod_element = ""
+	power_start_idx = 0
+	power_end_idx = 0
+	possible_power = False
+	found_power = False
+	power_str = ""
+
+	for idx, letter in enumerate(element):
+		# get literal
+		if letter not in NORMAL_CHARS and letter not in NUM_CHARS and letter != POWER and not found_power and not possible_power:
+			possible_power = True
+			power_start_idx = idx
+
+		if not possible_power:
+			mod_element += letter
+
+		if possible_power and not found_power and (letter in NORMAL_CHARS or idx==len(element)-1):
+			possible_power = False
+			mod_element += element[power_start_idx:idx]+letter
+
+		if letter == POWER:
+			found_power = True
+			power_end_idx = idx
+
+		# get power and replace
+		if found_power and letter in NUM_CHARS:
+			power_str += letter
+		if found_power and ((letter not in NUM_CHARS and letter != POWER) or (idx == len(element)-1)):
+			# replace
+			power = int(power_str)
+			replaced = "*".join([element[power_start_idx:power_end_idx] for i in range(power)])
+			if idx != len(element)-1 or letter not in NUM_CHARS:
+				replaced += letter
+			mod_element += replaced
+			possible_power = False
+			found_power = False
+			power_str = ""
+
+	return mod_element
+
+def change_var_names(element):
+	"""
+	Takes a Jacobian element and replaces Matlab variable
+	names by its Hackflight counterparts.
+
+	This method should be called after replace_powers to avoid conflicts
+	"""
+
+	mod_element = ""
+	var_start_idx = 0
+	var_end_idx = 0
+
+	found_var = False
+	possible_var = False
+
+	for idx, letter in enumerate(element):
+		# get literal
+		if letter not in NORMAL_CHARS+NUM_CHARS and not possible_var:
+			# hit literal
+			possible_var = True
+			var_start_idx = idx
+
+		if not possible_var:
+			mod_element += letter
+
+		# end of literal
+		if possible_var and (letter in NUM_CHARS+NORMAL_CHARS or idx == len(element)-1):
+			var_end_idx = idx
+			found_var = True
+
+		if found_var:
+			if idx != len(element)-1:
+				mod_element += SUBS[element[var_start_idx:var_end_idx]]+letter
+			else:
+				if letter not in NORMAL_CHARS+NUM_CHARS:
+					mod_element += SUBS[element[var_start_idx:var_end_idx]+letter]
+				else:
+					mod_element += SUBS[element[var_start_idx:var_end_idx]]+letter
+			possible_var = False
+			found_var = False
+
+	return mod_element
